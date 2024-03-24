b91ecda8936f72d7535d2ab99cc2c88e48ff821f
mariana lainara <marilainarasilva@hotmail.com>
2024-03-24 17:50:00 -0300

    First Commit

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author maril
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
